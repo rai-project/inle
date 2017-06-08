1d77f16a9f69040fc2bccd8f09214f0c624afaa3
Abdul Dakkak <abduld@wolfram.com>
2017-06-08 19:36:55 +0100

    fix for https://github.com/sirupsen/logrus/blob/master/README.md\#case-sensitivity

--- pkg/display/init.go
+++ pkg/display/init.go
@@ -1,7 +1,7 @@
 package display
 
 import (
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 
 	"github.com/rai-project/config"
 	logger "github.com/rai-project/logger"
--- pkg/magic/init.go
+++ pkg/magic/init.go
@@ -1,7 +1,7 @@
 package magic
 
 import (
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 
 	"github.com/rai-project/config"
 	logger "github.com/rai-project/logger"
